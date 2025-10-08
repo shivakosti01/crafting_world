cfabbb73dbf02fe529efe6e5f5f297fcc8ef9ada
Shiva Kosti <156178197+shivakosti01@users.noreply.github.com>
2025-10-08 15:04:44 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -52,7 +52,7 @@ router.post(
 router.get("/logout", function (req, res, next) {
   req.logout(function (err) {
     if (err) return next(err);
-    res.redirect("/index");
+    res.redirect("/");
   });
 });
 
