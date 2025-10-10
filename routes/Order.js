aede775b9cdcd739c98317a3f131bd656149ec75
Shiva Kosti <156178197+shivakosti01@users.noreply.github.com>
2025-10-10 11:53:59 +0530

    Update Order.js

--- routes/Order.js
+++ routes/Order.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-
+const plm = require("passport-local-mongoose");
+mongoose.connect('mongodb+srv://aasijain594_db_user:UulAHQubJiTb9uK1@cluster0.dkk9xjh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 const orderSchema = new mongoose.Schema({
   name: String,
   email: String,
